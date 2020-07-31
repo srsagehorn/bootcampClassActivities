a690b4ba3da13d7020e2fbccaf7e52d2d399b772
Shannon Sagehorn <srsagehorn@gmail.com>
2020-07-30 23:51:33 -0500

    employee tracker

---
+++ mvc/08-Wishes/server.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "rootroot",
+  database: "wishes_db",
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+app.get("/", function (req, res) {
+  connection.query("SELECT * FROM wishes;", function (err, data) {
+    if (err) throw err;
+    res.render("index", { wishes: data });
+  });
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
