55aaf9482a386b2f68fafe213df2d310ef150ca6
Shannon Sagehorn <srsagehorn@gmail.com>
2020-09-18 23:45:25 -0500

    testing

--- oOP.10/12-Stu_Structured-Tests/Unsolved/test/child.test.js
+++ oOP.10/12-Stu_Structured-Tests/Unsolved/test/child.test.js
@@ -1,5 +1,25 @@
 const Child = require("../child");
 
 describe("Child", () => {
-  
+  it("Should throw an error for an empty name", () => {
+    const name = "";
+    const age = 2;
+    expect(new Child(name, age)).toThrow(
+      new Error("Expected parameter 'name' to be a non-empty string")
+    );
+  });
+
+  it("Should throw an error for a negative number", () => {
+    const name = "Shannon";
+    const age = -5;
+    expect(new Child(name, age)).toThrow(
+      new Error("Expected parameter 'age' to be a non-negative number")
+    );
+  });
+
+  it("Should create a new child", () => {
+    const name = "Shannon";
+    const age = 21;
+    expect(new Child(name, age)).toBeInstanceOf(Child);
+  });
 });
